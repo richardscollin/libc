fc5efb5462a6afd10546f61e2a74af55989c49cc
Collin Richards <richardscollin@gatech.edu>
2015-08-14 01:57:20 -0500

    Misc changes
    Add include guard to stdlib.h
    Clean up stdlib.c includes
    Define NULL in stdlib.h
    Only compile in llabs if C99

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,4 +1,11 @@
 
+#ifndef __stdlib_h
+#define __stdlib_h
+
+#ifndef NULL
+#define NULL ((void*) 0)
+#endif
+
 struct div_t {
     int quot;
     int rem;
@@ -12,3 +19,4 @@ void *bsearch(const void *key, const void *base,
         size_t nmemb, size_t size,
         int (*compar)(const void *, const void *));
 
+#endif
--- src/stdlib.c
+++ src/stdlib.c
@@ -1,11 +1,6 @@
-
-#include <stdlib.h>
 #include <ctype.h>
 #include <stdint.h>
-
-#include <math.h>
-
-#include <string.h>
+#include <stdlib.h>
 
 double strtod(const char *s, char **endptr)
 {
@@ -108,8 +103,10 @@ long int labs(long int j)
 {
     return (j < 0) ? -j : j;
 }
+#if __STDC_VERSION__ >= 199901L
 long long int llabs(long long int j)
 {
     return (j < 0) ? -j : j;
 }
+#endif
 
